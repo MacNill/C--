dbeb3c353ccc3ab6a8a3a57c371dedcdcd6b0ded
Madina Alieva <amedi1606@icloud.com>
2023-03-30 23:36:53 +0300

    Добавлен новый репозиторий

---
+++ Examples/Program.cs
@@ -0,0 +1,65 @@
+﻿/*int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//              0    1   2   3
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+-----------------------------------------
+
+
+int [] array = {1,2,3,4,15,16,17,18};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1;
+    index++;
+}
+*/
+
+
+void FillArray(int[] collection)
+
+
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray (array);
