144ef60ce3166a2dea10a102b0bfdb4a68e4c4ad
Madina Alieva <amedi1606@icloud.com>
2023-04-25 21:57:04 +0300

    Задание 9

---
+++ Homework_9/Program.cs
@@ -0,0 +1,97 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+
+int i = InputNumbers("Введите значение N: ");
+int count = 2;
+PrintNumber(i, count);
+Console.Write(1);
+
+void PrintNumber(int i, int count)
+{
+    if (count > i) return;
+    PrintNumber(i, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+--------------------------------------------------
+
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке 
+от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+------------------------------------
+
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
